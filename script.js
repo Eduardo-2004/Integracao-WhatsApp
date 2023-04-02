26b004928bde696de215998a2d4f63cff6312ab0
Eduardo Sousa <eduardoeusou1@gmail.com>
2023-04-02 12:54:42 -0300

    update files

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const enviarForms = () => {
     let nome = document.getElementById('nome').value
     let sobrenome = document.getElementById('sobrenome').value
     let mensagem = document.getElementById('msg').value
-    let numero = 5511943560779
+    let numero = [your-number]
 
     var win = window.open(`https://wa.me/${numero}?text=Olá,%20meu%20nome%20é%20${nome}%20${sobrenome}, assunto: %20${mensagem}`,'_blank')
 }
